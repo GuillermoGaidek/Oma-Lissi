6b526ed8d6b91ec3f3f13c93435fea170e240dab
Guille <guillegaidek@hotmail.com>
2021-04-17 22:33:03 -0300

    ..

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -1,10 +1,9 @@
 let db = require("../database/models");
-//const { QueryTypes } = require("sequelize");
+const { QueryTypes } = require("sequelize");
 
 let reservasController = {
     crear: async function(req,res) {
-        const reservas = await db.sequelize.query("SELECT * FROM oma_lissi.reservas");
-        //,{ type: QueryTypes.SELECT });
+        const reservas = await db.sequelize.query("SELECT * FROM oma_lissi.reservas",{type:     QueryTypes.SELECT});
         return res.render("listadoReservas",{reservas:reservas});
     }
 }
